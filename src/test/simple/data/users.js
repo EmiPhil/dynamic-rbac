390bb96b521ab43670630bd3bb243d2057ef3af9
Philippe Larochelle <phil@emiphil.com>
2017-08-29 16:57:24 -0600

    Created Simple Use Case

---
+++ src/test/simple/data/users.js
@@ -0,0 +1,28 @@
+const users = {
+  '00001': {
+    name: 'Hekate Kyriakos',
+    title: 'Manager'
+  },
+  '00002': {
+    name: 'Plato Georgios',
+    title: 'Editor'
+  },
+  '00003': {
+    name: 'Telesphorus Harmonia',
+    title: 'Journalist'
+  },
+  '00004': {
+    name: 'Dionysus Antipater',
+    title: 'Journalist'
+  },
+  '00005': {
+    name: 'Alexius Demokritos',
+    title: 'Journalist'
+  },
+  '00006': {
+    name: 'Thaïs Lycurgus',
+    title: 'Journalist'
+  }
+}
+
+module.exports = users
